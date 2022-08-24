60d599eca0fda2ca18460693d5ea04f0f6b056f5
bayc <christopher.j.bay@gmail.com>
2022-08-23 23:11:00 -0600

    adding setup.py file to allow installation of repo

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='WindIO',
+      version='1.0',
+      description='IEA Wind Task 37 - ontology',
+      url='https://github.com/IEAWindTask37/windIO',
+      packages=[],
+      )
