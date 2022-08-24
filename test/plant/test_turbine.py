14830415ebbc6ceed8f4a9c2d331850ec7e88ed9
bayc <christopher.j.bay@gmail.com>
2022-08-23 23:14:07 -0600

    adding plant ontology tests

---
+++ test/plant/test_turbine.py
@@ -0,0 +1,16 @@
+from utils.pywake_utils import yml2WindTurbines
+from utils import plant_examples_data_path
+from py_wake.examples.data.iea37._iea37 import IEA37_WindTurbines
+import numpy.testing as npt
+import numpy as np
+
+
+def test_3_350_turbine():
+    ref = IEA37_WindTurbines()
+    wt = yml2WindTurbines(plant_examples_data_path + "plant_energy_turbine/IEA37_3.35MW_turbine.yaml")
+
+    npt.assert_equal(wt.diameter(), ref.diameter())
+    npt.assert_equal(wt.hub_height(), ref.hub_height())
+    u = np.linspace(0, 30, 100)
+    npt.assert_array_equal(wt.power(u)[0], ref.power(u)*1e3)
+    npt.assert_array_almost_equal(wt.ct(u), ref.ct(u))
