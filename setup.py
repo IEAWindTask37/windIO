a4c4d967e70dacbd1746fd8c90b80bded82ecb2a
pibo <ptrbortolotti@gmail.com>
2023-02-09 22:22:09 -0700

    extend setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='WindIO',
-      version='1.0',
-      description='IEA Wind Task 37 - ontology',
-      url='https://github.com/IEAWindTask37/windIO',
-      packages=[],
-      )
+metadata = dict(
+    name="windIO",
+    url='https://github.com/IEAWindTask37/windIO',
+    version="1.0",
+    description="Frameworks defining the inputs and outputs for systems engineering MDAO of wind turbine and plants.",
+    author="IEA Wind Task 37",
+    packages=setuptools.find_packages(exclude=["test", "examples"]),
+    python_requires=">=3.7",
+    zip_safe=True,
+    install_requires=['jsonschema', 'numpy', 'pyyaml', 'pytest', 'xarray']
+    )
+
+setuptools.setup(**metadata)
