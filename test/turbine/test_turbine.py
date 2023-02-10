a95e6af9a94a5e7a5535ab11f18112724f4df21e
pibo <ptrbortolotti@gmail.com>
2023-02-09 21:35:25 -0700

    fix test turbine

--- test/turbine/test_turbine.py
+++ test/turbine/test_turbine.py
@@ -4,30 +4,42 @@ from jsonschema import validate
 import yaml
 
 
-path2schema = os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) + os.sep + 'windIO' + os.sep + 'turbine' + os.sep + "IEAontology_schema.yaml"
-
+path2schema = (
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    + os.sep
+    + "windIO"
+    + os.sep
+    + "turbine"
+    + os.sep
+    + "IEAontology_schema.yaml"
+)
 
 
 class TestRegression(unittest.TestCase):
-    
     def test_IEA_3_4_130_RWT(self):
-        
-        path2yaml = os.path.dirname( os.path.realpath(__file__) ) + os.sep + "turbine_example.yaml"
+        path2yaml = (
+            os.path.dirname(os.path.realpath(__file__))
+            + os.sep
+            + "turbine_example.yaml"
+        )
         # Read the input yaml
-        with open(path2yaml, 'r') as myfile:
+        with open(path2yaml, "r") as myfile:
             inputs = myfile.read()
 
         # Read the schema
-        with open(path2schema, 'r') as myfile:
+        with open(path2schema, "r") as myfile:
             schema = myfile.read()
 
         # Run the validate class from the jsonschema library
-        validate(yaml.load(inputs, Loader=yaml.FullLoader), yaml.load(schema, Loader=yaml.FullLoader))
+        validate(
+            yaml.load(inputs, Loader=yaml.FullLoader),
+            yaml.load(schema, Loader=yaml.FullLoader),
+        )
 
         # Move it to a dictionary called wt_data
         wt_data = yaml.load(inputs, Loader=yaml.FullLoader)
 
-        return None 
+        return None
 
 
 def suite():
@@ -35,7 +47,6 @@ def suite():
     suite.addTest(unittest.makeSuite(TestRegression))
     return suite
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.TextTestRunner().run(suite())
-    
-    
