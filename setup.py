ca3937d117b07f314134585e998fd5af8c520b2c
Rafael M Mudafort <rafmudaf@gmail.com>
2023-09-03 15:51:33 -0600

    Move utils package into windIO

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 import setuptools
 
+# Required packages to use the library
+REQUIRED = [
+    'jsonschema',
+    'numpy',
+    'pyyaml',
+    'xarray',
+]
+
+# Optional packages
+# To use:
+#   pip install -e ".[test]"         installs testing packages in editable install
+#   pip install "windIO[test]"       installs testing packages in non-editable install
+EXTRAS = {
+    "test": {
+        'pytest',
+        'py_wake',
+        'topfarm',
+    },
+}
+
 metadata = dict(
     name="windIO",
     url='https://github.com/IEAWindTask37/windIO',
@@ -9,7 +29,8 @@ metadata = dict(
     packages=setuptools.find_packages(exclude=["docs*", "examples*", "test*"]),
     python_requires=">=3.7",
     zip_safe=True,
-    install_requires=['jsonschema', 'numpy', 'pyyaml', 'pytest', 'xarray'],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     license="Apache-2.0",
 )
 
