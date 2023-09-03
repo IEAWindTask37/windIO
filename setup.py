bc4906d7a8019451825cc1713d3f9de569133b39
Rafael M Mudafort <rafmudaf@gmail.com>
2023-09-03 15:29:30 -0600

    Prevent installing test directory

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ metadata = dict(
     version="1.0",
     description="Frameworks defining the inputs and outputs for systems engineering MDAO of wind turbine and plants.",
     author="IEA Wind Task 37",
-    packages=setuptools.find_packages(exclude=["test", "examples"]),
+    packages=setuptools.find_packages(exclude=["docs*", "examples*", "test*"]),
     python_requires=">=3.7",
     zip_safe=True,
-    install_requires=['jsonschema', 'numpy', 'pyyaml', 'pytest', 'xarray']
-    )
+    install_requires=['jsonschema', 'numpy', 'pyyaml', 'pytest', 'xarray'],
+    license="Apache-2.0",
+)
 
 setuptools.setup(**metadata)
